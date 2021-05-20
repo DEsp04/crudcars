7470c05f87f54387e0268e4b696564ebb2fb01af
david espinal <despinal0425@gmail.com>
2021-05-20 14:30:16 -0400

    added the path to the different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
 import './App.css';
 import { Route, withRouter } from "react-router-dom";
-import Car from "./components/routes/CarBrand";
-import createCarModel from "./components/routes/CreateCarModel";
-import createCarBrand from "./components/routes/CreateCarBrand";
+import CarModelEdit from "./components/routes/edit/EditCarModel"
+import CarBrandEdit from "./components/routes/edit/EditCarBrand"
 import Home from "./components/routes/Home";
-import EditCar from "./components/routes/edit/EditCarModel";
+import CarBrandOrModelCreate  from "./components/routes/create/ModelOrBrandCreate"
 import CarBrands from "./components/routes/CarBrands";
 import CarBrand from "./components/routes/CarBrand";
+import CarModel from "./components/routes/CarModel"
 
 
 
 
-function App(props) {
+function App() {
   return (
     <div className="App">
       <h3></h3>
       <Route exact path="/" component={Home} />
+      
       <Route exact path="/car-Brands" component={CarBrands} />
 
       <Route exact path="/add-car-brand-or-model" component={CarBrandOrModelCreate} />
 
-
-
-      <Route exact path="/car-models/:id/edit-car-model" component={CarModeEdit} />
-      <Route exact path="/car-brands/:id/edit-car-brand" component={CarBrandsEdit} />
+      <Route exact path="/car-models/:id/edit-car-model" component={CarModelEdit} />
       
-
+      <Route exact path="/car-brands/:id/edit-car-brand" component={CarBrandEdit} />
+      
       <Route exact path="/car-brands/:id/car-models" component={CarBrand} />
+      
       <Route exact path="/car-brands/:id/car-models/:id" component={CarModel} />
       
     </div>
